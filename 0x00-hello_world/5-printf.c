b0b1a47fc40e2caf85ed2aceea06319354d5e93a
Leonard Nzekwe <nzekweleo@gmail.com>
2023-03-16 16:20:17 -0700

    added 5-printf.c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	/* 
+	/**
 	 * printf  - prints the formatted string
 	 */
-	printf("%s\n" "with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
