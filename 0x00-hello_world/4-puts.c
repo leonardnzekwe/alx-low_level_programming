a643e3e779607e926566260a04f2b39b8914c71d
Leonard Nzekwe <nzekweleo@gmail.com>
2023-03-16 15:33:47 -0700

    added 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@
 int main(void)
 {
 	/*prints the string*/
-	fputs("Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
