645f75e442999445ab625b9e8871cfbffd22e54f
Leonard Nzekwe <nzekweleo@gmail.com>
2023-03-17 01:14:13 -0700

    added 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * write - writes to the std error
+ * return - 1
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
