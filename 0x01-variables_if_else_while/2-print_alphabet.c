eaf4acd261a31016c60abd4b56b24c1bf45ac9b0
Leonard Nzekwe <nzekweleo@gmail.com>
2023-03-17 05:06:42 -0700

    added 2-print_alphabet.c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * putchar - prints to the output
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	return (0)
+}
