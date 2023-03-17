bda957375b2071784528019def831ad399a51007
Leonard Nzekwe <nzekweleo@gmail.com>
2023-03-17 09:10:58 -0700

    added 9-print_comb.c file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,9 +9,13 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		putchar(num + '0');
+		if (num == 9)
+		{
+			continue;
+		}
 		putchar(',');
 		putchar(' ');
 	}
