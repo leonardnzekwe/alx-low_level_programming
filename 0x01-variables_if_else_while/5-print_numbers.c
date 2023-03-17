ece6937a07c2ee35915c83381f71fc5b439d411c
Leonard Nzekwe <nzekweleo@gmail.com>
2023-03-17 06:22:58 -0700

    added 5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prinf - Prints the output
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char num;
+
+	while (num <= 9)
+	{
+		printf('%c' num);
+	}
+	purchar('\n');
+	return (0);
+}
