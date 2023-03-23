4e5ae3e7342254f40271439f14642a1d1a75fabf
Leonard Nzekwe <nzekweleo@gmail.com>
2023-03-23 16:44:16 -0700

    added 1-isdigit.c file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - function
+ * @c: parameter
+ * Return: 1 ? 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
