8b3682507d3c2cd990afa8bad421f5aef489ea0a
Leonard Nzekwe <nzekweleo@gmail.com>
2023-03-23 16:37:09 -0700

    added 0-isupper.c file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - function
+ * @c: parameter
+ * Return: 1 ? 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
