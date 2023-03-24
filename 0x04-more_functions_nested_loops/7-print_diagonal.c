660172e8f4fcb7834e8024b4e59c9872f9330778
Leonard Nzekwe <nzekweleo@gmail.com>
2023-03-23 19:29:38 -0700

    added 7-print_diagonal.c file

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,7 @@
 
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n <= 0)
 	{
@@ -18,8 +18,12 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
+			for (j = 0; j < i: j++)
+			{
+				_putchar(32);
+			}
 			_putchar(92);
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
