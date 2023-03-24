2f877a341edad9bb9bc6e3c44f0ee09fb6dfb53c
Leonard Nzekwe <nzekweleo@gmail.com>
2023-03-23 17:17:32 -0700

    added 4-print_most_numbers.c file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i <+ 9)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
