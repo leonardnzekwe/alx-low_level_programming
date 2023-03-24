2c99dc8818d8e8bf1cd0f67788610d14527f1ab2
Leonard Nzekwe <nzekweleo@gmail.com>
2023-03-23 19:23:26 -0700

    added 7-print_diagonal.c file

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_line - function
+ * print_diagonal - function
  * @n: param
  * Return: 1
  */
 
-void print_line(int n)
+void print_diagonal(int n)
 {
 	int i;
 
@@ -18,7 +18,7 @@ void print_line(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			_putchar(95);
+			_putchar(92);
 		}
 		_putchar('\n');
 	}
