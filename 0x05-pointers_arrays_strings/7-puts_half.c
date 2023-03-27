d65f057056d159cf1d5352c203fd0a85bbb9ddb5
Leonard Nzekwe <nzekweleo@gmail.com>
2023-03-27 15:19:50 -0700

    added 7-puts_half.c file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - function
+ * @str: param
+ */
+
+void puts_half(char *str)
+{
+	int leo = 0;
+	int ray;
+
+	while (str[leo] != '\0')
+	{
+		leo++;
+	}
+	if (leo % 2 == 0)
+	{
+		ray = leo / 2;
+	}
+	else
+	{
+		ray = (leo + 1) / 2;
+	}
+	while (ray < leo)
+	{
+		_putchar(str[leo]);
+	}
+	_putchar('\n');
+}
