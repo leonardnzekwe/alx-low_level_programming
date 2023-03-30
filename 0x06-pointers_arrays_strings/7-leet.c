248f822d578bc168be36f35eb982f8ee96285d62
Leonard Nzekwe <nzekweleo@gmail.com>
2023-03-30 09:26:03 -0700

    added 7-leet.c file

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+/**
+ * leet - function
+ * @ray: param
+ * Return: ray
+ */
+
+char *leet(char *ray)
+{
+	int a = 0;
+	int b;
+	char leo[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	char ola[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+
+	while (ray[a])
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (ray[a] == ola[b])
+			{
+				ray[a] = leo[b];
+			}
+		}
+		a++;
+	}
+	return (ray);
+}
