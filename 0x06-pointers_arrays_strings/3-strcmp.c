8e2a263ff18d257787a31891d80e04bdc5288066
Leonard Nzekwe <nzekweleo@gmail.com>
2023-03-30 11:15:12 -0700

    added 3-strcmp.c file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,19 +4,46 @@
  * _strcmp - function
  * @s1: param
  * @s2: param
- * Return: 0
+ * Return: d
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2)
-	{
-		if (*s1 != *s2)
-		{
-			return (*s1 - *s2);
-		}
-		*s1++, *s2++;
-	}
-	return (*s1 - *s2);
-}
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	int ray;
 
+	while (s1[a])
+	{
+		a++;
+	}
+	while (s2[b])
+	{
+		b++;
+	}
+	if (a <= b)
+	{
+		ray = a;
+	}
+	else
+	{
+		leo = b;
+	}
+	while (c <= leo)
+	{
+		if (s1[c] == s2[c])
+		{
+			c++;
+			continue;
+		}
+		else
+		{
+			d = s1[c] - s2[c];
+			break;
+		}
+		c++;
+	}
+	return (d);
+}
