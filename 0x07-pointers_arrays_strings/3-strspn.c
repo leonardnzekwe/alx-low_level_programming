7ce8541f73b98a70021fa21c9d701cd8b37af3a0
Leonard Nzekwe <nzekweleo@gmail.com>
2023-04-03 14:20:33 -0700

    added 3-strspn.c file

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,7 +12,7 @@ unsigned int _strspn(char *s, char *accept)
 	int leo, ray, count;
 
 	count = 0;
-	for (leo = 0; s[leo] != ','; leo++)
+	for (leo = 0; s[leo] != ' '; leo++)
 	{
 		for (ray = 0; accept[ray] != '\0'; ray++)
 		{
