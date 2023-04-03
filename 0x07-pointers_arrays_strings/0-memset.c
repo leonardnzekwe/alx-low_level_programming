097430489d1a16e7eba84c11b5359516cd343be8
Leonard Nzekwe <nzekweleo@gmail.com>
2023-04-03 02:13:06 -0700

    added 0-memset.c file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * memset - function
+ * @s: param
+ * @b: param
+ * n: param
+ * Return: s
+ */
+
+char *memset(char *s, char b, unsigned int n)
+{
+	int leo;
+
+	for (leo = 0; leo < n; leo++)
+	{
+		s[leo] = b;
+		_putchar(*s);
+	}
+	return (s);
+}
