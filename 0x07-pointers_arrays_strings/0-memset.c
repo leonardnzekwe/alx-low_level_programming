6de9ae971716f3c2fd93c468d2192047629c8eb8
Leonard Nzekwe <nzekweleo@gmail.com>
2023-04-03 05:37:05 -0700

    added 0-memset.c file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * memset - function
+ * _memset - function
  * @s: param
  * @b: param
  * @n: param
  * Return: s
  */
 
-char *memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int leo;
+	int leo;
 
 	for (leo = 0; leo < n; leo++)
 	{
