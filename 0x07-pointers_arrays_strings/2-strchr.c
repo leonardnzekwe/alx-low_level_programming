c30fcd2aec88e84bfdac695c113b93fa00dd2881
Leonard Nzekwe <nzekweleo@gmail.com>
2023-04-03 06:30:34 -0700

    added 2-strchr.c file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - function
+ * @s: param
+ * @c: param
+ */
+
+char *_strchr(char *s, char c)
+{
+	int leo;
+
+	for (leo = 0; s[leo] != '\0'; leo++)
+	{
+		if (s[leo] == c)
+		{
+			return (s + leo);
+		}
+	}
+}
