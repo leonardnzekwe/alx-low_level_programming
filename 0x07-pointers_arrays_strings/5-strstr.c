eaafa16e12586a71940c01dcf7ae9124c70b5b69
Leonard Nzekwe <nzekweleo@gmail.com>
2023-04-03 16:58:23 -0700

    added 5-strstr.c file

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,7 +11,7 @@ char *_strstr(char *haystack, char *needle)
 {
 	int leo;
 
-	for (leo = 0; haystack[leo] >= '\0'; leo++)
+	for (leo = 0; haystack[leo]; leo++)
 	{
 		if (needle[0] == haystack[leo])
 		{
