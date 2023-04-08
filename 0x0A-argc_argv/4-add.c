4fd3c9bc10bdb9587c5a09f89b2fd0f0988cbeac
Leonard Nzekwe <nzekweleo@gmail.com>
2023-04-08 07:22:29 -0700

    added 4-add.c file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function
+ * @argc: param
+ * @argv: param
+ * Return: 0 ? 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int ray;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else if (argc > 1)
+	{
+		for (ray = 1; ray < argc; ray++)
+		{
+			sum = sum + atoi(argv[ray]);
+			if (*argv[ray] < 48 || *argv[ray] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
