645471b2ded0da527f520e239957874f0c8e1f2e
Leonard Nzekwe <nzekweleo@gmail.com>
2023-04-11 15:29:51 -0700

    added 3-alloc_grid.c file

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - function
+ * @width: param
+ * @height: param
+ * Return: p
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int leo, ray;
+	int **p;
+
+	if (height < 1)
+	{
+		return (NULL);
+	}
+	p = (int **) malloc(sizeof(int) * height);
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	for (leo = 0; leo < height; leo++)
+	{
+		p[leo] = malloc(sizeof(int) * width);
+		if (p[leo] == NULL)
+		{
+			for (ray = 0; ray < leo; ray++)
+			{
+				free(p[ray]);
+			}
+			free(p);
+			return (NULL);
+		}
+
+		for (ray = 0; ray < width; ray++)
+		{
+			p[leo][ray] = 0;
+		}
+	}
+	return (p);
+}
