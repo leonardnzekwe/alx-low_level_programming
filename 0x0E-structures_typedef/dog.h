d1ae05fad6131a4d2185b85228c9186505903f2a
Leonard Nzekwe <nzekweleo@gmail.com>
2023-04-17 14:44:30 -0700

    added dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct DOG - Dog structure
+ * @name: member
+ * @age: member
+ * @owner: member
+ *
+ * Description: Structure of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
