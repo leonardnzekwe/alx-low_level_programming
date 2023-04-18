18cc625a733b1cfb9495ae90d03ab4bec860c9e6
Leonard Nzekwe <nzekweleo@gmail.com>
2023-04-17 20:01:38 -0700

    added 4-new_dog.c file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,20 +14,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int a, b, c;
 	dog_t *daisy;
 
-	daisy = malloc(sizeof(daisy));
-	if (daisy == NULL || !(name) || !(owner))
+	daisy = malloc(sizeof(dog_t));
+	if (daisy == NULL)
+		return (NULL);
+	if (name == NULL || owner == NULL || name[0] == '\0' || owner[0] == '\0')
 	{
 		free(daisy);
 		return (NULL);
 	}
-
 	for (a = 0; name[a]; a++)
 		;
 	for (b = 0; owner[b]; b++)
 		;
-
-	(*daisy).name = malloc(sizeof(dog_t) * a + 1);
-	(*daisy).owner = malloc(sizeof(dog_t) * b + 1);
+	(*daisy).name = malloc(sizeof(char) * (a + 1));
+	(*daisy).owner = malloc(sizeof(char) * (b + 1));
 	if (!((*daisy).name) || !((*daisy).owner))
 	{
 		free((*daisy).owner);
@@ -35,17 +35,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(daisy);
 		return (NULL);
 	}
-
 	for (c = 0; c < a; c++)
-	{
 		(*daisy).name[c] = name[c];
-	}
 	(*daisy).name[c] = '\0';
-	(*daisy).age = age;
 	for (c = 0; c < b; c++)
-	{
 		(*daisy).owner[c] = owner[c];
-	}
 	(*daisy).owner[c] = '\0';
+	if (age < 0)
+	{
+		free((*daisy).owner);
+		free((*daisy).name);
+		free(daisy);
+		return (NULL);
+	}
+	(*daisy).age = age;
 	return (daisy);
 }
