3718eb035d1112d6fe49774748cfaf77e9f765ec
Leonard Nzekwe <nzekweleo@gmail.com>
2023-04-17 17:07:26 -0700

    added 4-new_dog.c file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,21 +14,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int a, b, c;
 	dog_t *daisy;
 
-	for (a = 0; name[a++];)
+	for (a = 0; name[a] != '\0'; a++)
 		;
-	for (b = 0; owner[b++];)
+	for (b = 0; owner[b] != '\0'; b++)
 		;
 	daisy = malloc(sizeof(dog_t));
 	if (daisy == NULL)
 		return (NULL);
 	(*daisy).name = malloc(sizeof((*daisy).name) * a);
-	if (daisy == NULL)
+	if ((*daisy).name == NULL)
 		return (NULL);
 	for (c = 0; c < a; c++)
 		(*daisy).name[c] = name[c];
 	(*daisy).age = age;
 	(*daisy).owner = malloc(sizeof((*daisy).owner) * b);
-	if (daisy == NULL)
+	if ((*daisy).owner == NULL)
 		return (NULL);
 	for (c = 0; c < b; c++)
 		(*daisy).owner[c] = owner[c];
