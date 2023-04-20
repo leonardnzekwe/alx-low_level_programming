c16ee23455c3babe9f0e5dcd5728e8e33423a789
Leonard Nzekwe <nzekweleo@gmail.com>
2023-04-20 13:34:55 -0700

    added 1-print_numbers.c file

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,11 +11,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (n == 0)
-	{
-		exit(-1);
-	}
-	else
+	if (n > 0)
 	{
 		va_start(args, n);
 		for (i = 0; i < n; i++)
