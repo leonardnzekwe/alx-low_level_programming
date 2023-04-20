80d2c127e305460cce9cb398d0045a4295c999df
Leonard Nzekwe <nzekweleo@gmail.com>
2023-04-20 04:53:26 -0700

    added 3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h files

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if (num2 == 0 && (*sign == '/' || *sign == '%'))
+	if ((num2 == 0 && *sign == '/') || (num2 == 0 && *sign == '%'))
 	{
 		printf("Error\n");
 		exit(100);
