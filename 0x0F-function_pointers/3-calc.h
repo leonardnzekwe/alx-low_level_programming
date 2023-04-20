ee567b27260d9397ea38a2cc50d966cd2ea186b3
Leonard Nzekwe <nzekweleo@gmail.com>
2023-04-20 04:23:45 -0700

    added 3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h files

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,8 @@
 #ifndef CALC_H
 #define CALC_H
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct op - Struct op
@@ -20,5 +23,6 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
 #endif /* CALC_H */
