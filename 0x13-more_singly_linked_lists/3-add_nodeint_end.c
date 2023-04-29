e2f3a8430f4bdea0bd55646449e045cc6ed06bf6
Leonard Nzekwe <nzekweleo@gmail.com>
2023-04-29 07:30:37 -0700

    Add Task Files, 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,6 +14,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *tail;
 
 	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 	(*new).n = n;
 	(*new).next = NULL;
 	if (*head == NULL)
