8239e1ee4072783a176cf02d09d2e9c2d082f4cf
Leonard Nzekwe <nzekweleo@gmail.com>
2023-04-30 07:32:17 -0700

    Add Task Files, 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if (*head == NULL)
+	{
+		return;
+	}
 	while (*head != NULL)
 	{
 		tmp = *head;
-		*head = (**head).next;
+		*head = (*tmp).next;
 		free(tmp);
 	}
 	*head = NULL;
