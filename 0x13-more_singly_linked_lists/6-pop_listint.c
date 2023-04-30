292dc40237bde03843b7b245f6346f095acf73e0
Leonard Nzekwe <nzekweleo@gmail.com>
2023-04-30 08:15:37 -0700

    Add Task Files, 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,7 +3,9 @@
 /**
  * pop_listint - singly linked list function
  * That deletes the head node of a listint_t linked list
+ * @head: listint_t head node passed as parameter
  * Return: the head node’s data (n)
+ * if the linked list is empty return 0
  */
 
 int pop_listint(listint_t **head)
@@ -11,7 +13,7 @@ int pop_listint(listint_t **head)
 	int data;
 	listint_t *tmp;
 
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		return (0);
 	}
