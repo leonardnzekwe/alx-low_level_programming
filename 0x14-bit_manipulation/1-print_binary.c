7e31ace7a9dc9273cd1e701ee18f724a0d86a45c
Leonard Nzekwe <nzekweleo@gmail.com>
2023-05-04 16:36:41 -0700

    Add Task Files, 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,34 +8,16 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int m;
-	int len;
-
-	if (n == 0)
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	if (n & 1)
+	{
+		_putchar('1');
+	}
+	else
 	{
 		_putchar('0');
-		return;
-	}
-	m = 1;
-	len = 0;
-	while ((n >> len) > 0)
-	{
-		len++;
-	}
-	if (len == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	for (len = len - 1; len >= 0; len--)
-	{
-		if ((n >> len) & m)
-		{
-			_putchar('1');
-		}
-		else
-		{
-			_putchar('0');
-		}
 	}
 }
