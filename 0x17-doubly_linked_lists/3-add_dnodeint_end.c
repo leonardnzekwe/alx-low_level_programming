6dde5b4bd3973ef8ab16975d24ea0e4876c187d2
Leonard Nzekwe <nzekweleo@gmail.com>
2023-06-15 17:32:50 -0700

    Add Files, task-3

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - double linked list function
+ * @head: node head
+ * @n: node data
+ * Return: the address of the new element, or NULL if it fails
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *ptr;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->prev = NULL;
+	new->n = n;
+	new->next = NULL;
+	
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		ptr = *head;
+		while (ptr->next != NULL)
+		{
+			ptr = ptr->next;
+		}
+		ptr->next = new;
+		new->prev = ptr;
+	}
+	return (new);
+}
