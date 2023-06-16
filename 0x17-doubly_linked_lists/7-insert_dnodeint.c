4653e279e459471c91ebf67fbca03c373ce02b76
Leonard Nzekwe <nzekweleo@gmail.com>
2023-06-15 19:33:59 -0700

    Add Files, task-7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,6 +16,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new;
 	unsigned int current = 1;
 
+	if (*h == NULL)
+		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
