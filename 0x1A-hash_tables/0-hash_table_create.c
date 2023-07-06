b180e93a960e04603a947ae1e477eed82f4a6b88
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-06 15:32:42 -0700

    Update Files, task-0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,13 +9,21 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	hash_node_t **array;
 
-	table = malloc(size);
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
+	array = malloc(sizeof(hash_node_t) * size);
+	if (array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
 	table->size = size;
-	table->array = NULL;
+	table->array = array;
 
 	return (table);
 }
