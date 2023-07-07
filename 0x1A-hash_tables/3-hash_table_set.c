20638559f4b2a414822e2eeef056467bbb9fd196
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-07 02:19:47 -0700

    Add Files, task-3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - a function that adds an element to the hash table
+ * @ht: is the hash table you want to add or update the key/value to
+ * @key: is the key. key can not be an empty string
+ * @value: is the value associated with the key
+ * Value must be duplicated. value can be an empty string
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	char *value_dup;
+	unsigned long int index;
+	hash_node_t *new_node;
+	hash_node_t *ptr;
+
+	if (strcmp(key, "") == 0 || ht == NULL)
+		return (0);
+
+	value_dup = strdup(value);
+	if (value_dup == NULL)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		free(value_dup);
+		return (0);
+	}
+
+	new_node->key = (char *)key;
+	new_node->value = value_dup;
+	new_node->next = NULL;
+
+	if (ht->array[index] == NULL)
+		ht->array[index] = new_node;
+	else
+	{
+		ptr = ht->array[index];
+		while (ptr->next != NULL)
+			ptr = ptr->next;
+		ptr->next = new_node;
+	}
+
+	free(value_dup);
+	return (1);
+}
