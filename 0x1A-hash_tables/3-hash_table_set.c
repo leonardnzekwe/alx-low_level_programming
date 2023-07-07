2c8cd2082f878422c24dbaa882236a8fc2bec360
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-07 03:25:21 -0700

    Update Files, task-3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node, *head, *ptr;
 	char *value_dup, *key_dup;
 
-	if (ht == NULL || key == NULL || strcmp(key, "") == 0 || value == NULL)
+	if (ht == NULL || strcmp(key, "") == 0)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	head = ht->array[index];
