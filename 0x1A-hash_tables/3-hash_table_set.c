151f276091c98e4ccb8bb826f44e22d11dae20b6
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-07 05:56:30 -0700

    Fix Error, checker

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -38,6 +38,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
+		return (0);
 	}
 	return (1);
 }
