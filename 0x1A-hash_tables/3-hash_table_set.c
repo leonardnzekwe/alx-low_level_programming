7a89ab7c0938abeaf859ba0437b83089c3d1f1aa
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-07 06:27:10 -0700

    Fix Error, checker

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node;
 
-	if (ht == NULL || key == NULL || strcmp(key, "") == 0 || value == NULL)
+	if (ht == NULL || strcmp(key, "") == 0)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
