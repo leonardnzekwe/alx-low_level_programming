cd94a2ff6cb97b2bb7dd89e4f815256cf94e61b2
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-07 06:10:13 -0700

    Fix Error, checker

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -39,8 +39,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			ptr->value = strdup(value);
 			if (ptr->value == NULL)
 				return (0);
-			return (1);
-		}
+		}	
 	}
 
 	if (ht->array[index] == NULL)
