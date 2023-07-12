45770844b5f9c27a0f8be696a92f3f105c03bfb7
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-12 08:35:53 -0700

    Update Files, recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,12 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
+	if (*s == '\0')
 	{
-		_putchar(s[i]);
-		i += 1;
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
