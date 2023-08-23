84aeb95a1223ac1b7077b2e8fcda01397c0ef60d
Leonard Nzekwe <nzekweleo@gmail.com>
2023-08-23 15:38:16 +0100

    Update Files, task-5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,36 +11,31 @@ def island_perimeter(grid):
     that returns the perimeter of the island described in grid
     """
     perimeter = 0
-    col_index = None
-    length = None
-    width = None
-    for row in grid:
-        for index, item in enumerate(row):
-            if item == 1:
-                col_index = index
-                break
-    if col_index:
-        length = get_length(grid, col_index)
+    if grid:
+        length = get_length(grid)
         width = get_width(grid)
-
-    if length and width:
         perimeter = (length * 2) + (width * 2)
     return perimeter
 
 
-def get_length(grid, col_index):
+def get_length(grid):
     """
     get_length function:
     that return the length of the grid
+    by turning the columns in the grid into rows,
+    getting the transposed row with the highest count of (1)
+    and then returning the count of (1) i.e top_width for that top_row
     """
-    row_list = []
     length = 0
-    for row in grid:
-        row_list.append(row[col_index])
-    if row_list:
-        length_top = row_list.index(1)
-        length_bottom = len(row_list) - 1 - row_list[::-1].index(1)
-        length = len(row_list[length_top:length_bottom + 1])
+    new_grid = []
+
+    for col_index in range(len(grid[0])):
+        new_row = []
+        for row_index in range(len(grid)):
+            new_row.append(grid[row_index][col_index])
+        new_grid.append(new_row)
+    if new_grid:
+        length = get_width(new_grid)
     return length
 
 
@@ -53,6 +48,7 @@ def get_width(grid):
     """
     top_width = -1
     top_row = -1
+
     for row_index, row in enumerate(grid):
         new_width = row.count(1)
         if new_width > top_width:
